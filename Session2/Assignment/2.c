946a7fb7499635a09f1a26f01a8210acae671951
sogol Haghani <42089295+sogolhaghani@users.noreply.github.com>
2018-11-09 17:50:07 +0330

    Add files via upload

---
+++ Session2/Assignment/2.c
@@ -0,0 +1,18 @@
+// خطاهای موجود در برنامه را پیدا و حل کنید.
+
+include <stdio>
+
+int main()
+{
+
+    int a;
+    printf("The value of a is %d\n", &a);
+    printf("The value of b is %d\n", b);
+    print("Enter two value for a and b");
+    scanf("%d%d", &a, b);
+
+    int .sum;
+    .sum <= a+b;
+    printf("sum value is : %f",.sum);
+    
+}
