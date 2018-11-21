cf89e70685cf04d6ccf431b093721b800adc35be
sogolhaghani <haghani.sogol@gmail.com>
2018-11-21 06:19:10 +0330

    Answer

--- Session2/Assignment/1.c
+++ Session2/Assignment/1.c
@@ -4,3 +4,35 @@
 // و 
 //  float
 // نیز باشند. 
+
+
+#include <stdio.h>
+
+int main(){
+
+/*********************Calculate with float Value***************************/
+    float radius_f= 0. , area_f = 0.0 , circumference_f = 0.0;
+    const float PI = 3.14;
+    printf("Please Enter value for Radius : \n");
+    scanf("%f", &radius_f);
+
+    circumference_f = 2 * radius_f * PI ;
+    area_f = radius_f * radius_f * PI ;
+
+    printf("Circumference = %f\n",circumference_f );
+    printf("Area = %f\n",area_f );
+/*********************Calculate with Double Value***************************/
+    double radius_d = 0.0, area_d = 0.0 , circumference_d = 0.0;
+
+
+    printf("Please Enter value for Radius : \n");
+    scanf("%lf", &radius_d);
+
+    circumference_d = 2 * radius_d * PI ;
+    area_d = radius_d * radius_d * PI ;
+
+    printf("Circumference = %lf\n",circumference_d );
+    printf("Area = %lf\n",area_d );    
+
+    return 0;
+}
