4ab88a0bb076bf32c766b6c77502f344abdca451
sogolhaghani <haghani.sogol@gmail.com>
2018-11-25 20:33:46 +0330

    Answered

--- Session3/Assignment/2.c
+++ Session3/Assignment/2.c
@@ -6,3 +6,51 @@
 از متغیر ثابت در برنامه استفاده شود.
 
 */
+
+
+#include <stdio.h>
+
+int main(){
+
+    const int total = 11;
+    printf("*** One member***\n");
+    for (int i = 1; i <= total ; i++) {
+        printf("{%d}",i);
+        if(i != total){
+            printf(",\t");
+        }else{
+            printf("\n");
+        }
+    }
+
+
+    printf("*** Two member***\n");
+    for (int i = 1; i <= total ; i++) {
+        for(int j = i+1; j <= total ; j++){
+            printf("{%d , %d}",i,j);
+            if(j != total){
+                printf(",\t");
+            }
+        }
+        printf("\n");
+    }   
+
+    printf("*** Three member***\n");
+    for (int i = 1; i <= total ; i++) {
+        for(int j = i+1; j <= total ; j++){
+            for(int k = j+1; k <= total ; k++){
+                printf("{%d , %d , %d}", i, j, k);
+                if(k != total){
+                    printf(",\t");
+                }
+            }
+           if(j != total && j != total -1){
+                printf(",\t");
+            }
+        }
+        printf("\n");
+    } 
+
+
+    return 0;
+}
--- Session3/Assignment/3.c
+++ Session3/Assignment/3.c
@@ -10,3 +10,43 @@
 از متغیر ثابت در برنامه استفاده شود.
 
 */
+
+
+#include <stdio.h>
+
+int main(){
+
+    int const TOTAL = 5;
+    int input = 0, min1 = 0, min2=0, index_min1 =0, index_min2 = 0;
+    printf("Please Enter 5 integer number\n");
+    scanf("%d",&input);
+    min1 = input;
+    index_min1 = 1;
+    scanf("%d",&input);
+    if(input <min1){
+        min2 = min1;
+        index_min2 = index_min1;
+        min1 = input;
+        index_min1 = 1;
+    }else{
+        min2 = input ;
+        index_min2 = 2;
+    }
+   
+    for(int i = 3 ; i <= TOTAL ; i++){
+        scanf("%d",&input);
+        if(input < min1){
+            min2 = min1;
+            index_min2 = index_min1;
+            min1 = input;
+            index_min1 = i;
+        }else if(input < min2){
+            min2 = input ;
+            index_min2 = i;
+        }
+    }
+
+    printf("Minimum second number is %d \n", min2);
+    printf("It is the %d you entered\n", index_min2);
+    return 0;
+}
