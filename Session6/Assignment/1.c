91a2471b0705299737f0420649c8485317239abf
sogolhaghani <haghani.sogol@gmail.com>
2018-12-20 17:47:52 +0330

    session6-answer

--- Session6/Assignment/1.c
+++ Session6/Assignment/1.c
@@ -25,3 +25,77 @@
 از آرایه استفاده شود
 ایرادهای برنامه گذشته تان نباید وجود داشته باشد.
 */
+#include <stdio.h>
+
+#define SIZE 4
+void printLine();
+void calculateAndPrintFactor(const int perItemPrices[4], int itemCount[4]);
+int main()
+{
+    int _continue = 1, code = 0, i = 0, count = 0;
+    const int price[SIZE] = {5000, 2000, 1000, 2500};
+    int itemCount[SIZE] = {0, 0, 0, 0};
+    while (_continue == 1)
+    {
+        printf("please enter code of item : 1, 2, 3, 4\n");
+        scanf("%d", &code);
+        if (code <= 4 && code >= 1)
+        {
+            printf("please enter number of item\n");
+            scanf("%d", &count);
+            itemCount[code - 1] += count;
+        }
+        else
+        {
+            printf("Wrong code!!!\n Try again...\n");
+        }
+
+        printf("Do you want to continue? 1 -> yes, other key-> no\n");
+        scanf("%d", &_continue);
+    }
+    calculateAndPrintFactor(price, itemCount);
+    return 0;
+}
+
+void calculateAndPrintFactor(const int perItemPrices[SIZE], int itemCount[SIZE])
+{
+    printf("\n\n");
+    printf("\t\tMY SHOPPING CENTER\n");
+    printf(" \t\tCASH BILL\n");
+    printLine();
+    printf("CODE\tCOUNT\tIETM PRICE\tTOTAL PRICE\n");
+    printLine();
+    int totalItem = 0, totalPrice = 0;
+    for (int i = 0; i < SIZE; i++)
+    {
+        if (itemCount[i] == 0)
+        {
+            continue;
+        }
+        totalItem += itemCount[i];
+        totalPrice += itemCount[i] * perItemPrices[i];
+        printf("%d\t", i + 1);
+        printf("%3d\t", itemCount[i]);
+        printf("%4d\t", perItemPrices[i]);
+        printf("%15d\n", perItemPrices[i] * itemCount[i]);
+    }
+    printLine();
+    printf("\t %d", totalItem);
+    printf(" \t\t\t%d\n", totalPrice);
+    if (totalPrice > 100000)
+    {
+        printLine();
+        printf("\t 15\%%");
+        printf(" \t\t\t%.0f\n", totalPrice * 0.85);
+    }
+}
+
+void printLine()
+{
+    const int line = 50;
+    for (int i = 0; i < line; i++)
+    {
+        printf("_");
+    }
+    printf("\n");
+}
